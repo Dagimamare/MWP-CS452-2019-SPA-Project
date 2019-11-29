35305d5b39ad291ca90b831a8d8ba7e9db301a9b
Dagimamare <amaretekle1@gmail.com>
2019-11-29 01:08:56 -0600

    My third day progress

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 window.onload = function () {
-
-    let frontPage = `<h1><b>Please login</b></h1>
+    let token,tid=0;
+    let frontPage = 
+    `<h1><b>Please login</b></h1>
     User Name <input type="text" id="userName"><br>
     Pass Word <input type="text" id="password"><br>
     <button type="button" id="lbut">Login</button>`;
 
-    let secondPage = `<textarea id="anima" cols="80" rows="20"></textarea> 
+    let secondPage = `
+     <div id="gioId"></div>
+    <textarea id="anima" cols="80" rows="20"></textarea> 
     <button id="refid">Refresh Animation</button>
     <button id="getApi">LogOut</button>`
-     
-    let myfirestPage=document.getElementById("outlet")
-    myfirestPage.innerHTML=frontPage;
+   
+
+    let myfirestPage = document.getElementById("outlet")
+    myfirestPage.innerHTML = frontPage;
 
     let loginButen = document.getElementById("lbut");
-    loginButen.addEventListener('click',forMySecondpage);
-    
+    loginButen.addEventListener('click', forMySecondpage);
 
+   
 
     function forMySecondpage() {
 
@@ -23,8 +27,9 @@ window.onload = function () {
 
         seclogBuf();
         refreshCl();
+        gioLocation()
 
-   }
+    }
     function seclogBuf() {
         let loginBu = document.getElementById("getApi");
         loginBu.addEventListener('click', function () {
@@ -34,37 +39,81 @@ window.onload = function () {
     }
 
     function refreshCl() {
-        let textarP = document.getElementById("anima")
+        //let textarP = document.getElementById("anima")
         let refBu = document.getElementById("refid");
-        refBu.addEventListener('click', function () {
-        textarP.innerHTML = aniData;
-        })
+        refBu.addEventListener('click',getFeach);
     }
- 
+    
     function fetchatOne() {
         fetch("http://mumstudents.org/api/login",
-        {
-                    method: 'POST', 
-                    headers: { "Content-Type": "application/json"},
-                    body: JSON.stringify({ username: "mwp",
-                     password:"123"})
-        })
-        .then(res=>res.json())
-        .then(data => {fetch("http://mumstudents.org/api/animation",{
-                        method: 'GET', 
-                        headers: {"Authorization": `Bearer ${data.token}` }
-                    
+            {
+                method: 'POST',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({
+                    username: "mwp",
+                    password: "123"
+                })
             })
-            .then(res=>res.text())
-            .then(data=>{
-                aniData=data.split("=====")
-                console.log(data)
+            .then(res => res.json())
+            .then(data => {token=data.token;
+                
+                getFeach() })
+            }
+        function getFeach() {
+            clearInterval(tid);
+            fetch("http://mumstudents.org/api/animation", {
+                method: 'GET',
+                headers: { "Authorization": `Bearer ${token}` }
+
             })
+                .then(res => res.text())
+                .then(data => {
+                    aniData = data.split("=====\n")
+
+               
+                    let count=0;               
+                    tid=setInterval(function(){
+                      document.getElementById("anima").innerHTML=aniData[count];
+                    count++;
+                    if(count==aniData.length){
+                        count=0}
+                    },200)
+                           
+                })
+                
+
+        }
+
+function gioLocation(){
+    fetch("http://www.mapquestapi.com/geocoding/v1/reverse?key=vhWCTTt1Zi5UypzejKx3B4rGTDLNxhkb&location=30.333472,-81.470448")
+    .then(res=>res.json())
+    .then(data=>{ let location=data.results[0].locations[0];
+                 
       
-      })
-    }
-  
-    fetchatOne();
-  
+     let gioDiv= document.getElementById("gioId");
+     gioDiv.innerHTML=`Well Come ${location.adminArea4} ,${location.adminArea3}, ${location.adminArea1}`
+    });
+}
+
+ function toupdatelocation(){
+    //let long=0;
+    //let lati=0;
+    function success(position) {
+        long= position.coords.longitude ;
+        lati= position.coords.latitude ;
+        console.log(long)
+        console.log(lati)
+
+        }
+        function fail(msg) {
+        console.log(msg.code + msg.message); // Log the error
+        }
+        navigator.geolocation.getCurrentPosition(success, fail);
+        
+
+ }
+   toupdatelocation()
+    fetchatOne()
+
 
 }
